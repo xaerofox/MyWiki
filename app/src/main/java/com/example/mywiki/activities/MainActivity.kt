32e8fd6f41eb09227aae0306ae18e1f961255e4a
Jose Torres <xaerofox@gmail.com>
2021-06-09 08:58:05 -0400

    Updated the content within the fragment layouts Updated MainActivity to utilize the new fragments

--- app/src/main/java/com/example/mywiki/activities/MainActivity.kt
+++ app/src/main/java/com/example/mywiki/activities/MainActivity.kt
@@ -10,12 +10,19 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mywiki.R
+import com.example.mywiki.activities.fragments.ExploreFragment
+import com.example.mywiki.activities.fragments.FavoritesFragment
+import com.example.mywiki.activities.fragments.HistoryFragment
 import com.example.mywiki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val exploreFragment: ExploreFragment = ExploreFragment()
+    private val favoritesFragment: FavoritesFragment = FavoritesFragment()
+    private val historyFragment: HistoryFragment = HistoryFragment()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -24,22 +31,44 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val textHome = findViewById<TextView>(R.id.text_home)
-        textHome.setOnClickListener { _ ->
-            startActivity(Intent(this, ArticleDetailActivity::class.java))
-        }
+//        val textHome = findViewById<TextView>(R.id.text_home)
+//        textHome.setOnClickListener {
+//            startActivity(Intent(this, ArticleDetailActivity::class.java))
+//        }
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
+        transaction.commit()
+
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        when(item.itemId)
+        {
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_favorites -> transaction.replace(R.id.fragment_container, favoritesFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
+        }
+
+        transaction.commit()
+
+        true
     }
 }
